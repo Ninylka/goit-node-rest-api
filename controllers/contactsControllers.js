ec1d6beb3a9dbb97df9c288b136dbaad90813c16
Ninylka <shemetylka@gmail.com>
2024-03-11 12:27:00 +0100

    first

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,15 +1,19 @@
 
 import ctrlWrapper from "../helpers/ctrlWrapper.js";
 import HttpError from "../helpers/HttpError.js";
-
 import Contact from "../models/contact.js";
 
  
 
 const getAllContacts = async (req, res) => {
-    const result = await Contact.find();
+    const {_id : owner} = req.user;
+    const {page = 1, limit = 20} = req.query;
+    const skip = (page - 1) * limit;
+    const result = await Contact.find({owner}, "-createdAt -updatedAt", {skip, limit});
     res.json(result);
+
 };
+
 
 
 const getOneContact = async (req, res) => {
@@ -26,14 +30,22 @@ const getOneContact = async (req, res) => {
 
 
 const createContact = async (req, res) => {
-    const result = await Contact.create(req.body);
+    const {_id : owner} = req.user;
+    const{email} = req.body;
+    const contact = await Contact.findOne({email});
+    if(contact){
+        throw(HttpError(409, "Email in use"))
+    }
+ 
+    const result = await Contact.create({...req.body, owner});
     res.status(201).json(result);
   };
 
 
 
+
 const updateContact = async (req, res) => {
-    const { id } = req.params;
+    const { id, } = req.params;
 
     const updatedContact = {
         ...req.body
@@ -65,8 +77,6 @@ const updateStatusContact = async (req, res) => {
 };
 
 
-
-
  const deleteContact = async(req, res) => {
     const {id} = req.params;
     const result = await Contact.findByIdAndDelete(id);
@@ -80,6 +90,19 @@ const updateStatusContact = async (req, res) => {
 };
 
 
+const getFavoriteContacts = async(req, res) =>{
+    const { favorite } = req.query;
+    const owner = req.user._id;
+    let contacts;
+        if (favorite === 'true') {
+            contacts = await Contact.find({ owner, favorite: true });
+        } else {
+            contacts = await Contact.find({ owner });
+        }
+
+        res.json(contacts);
+}
+
 
 export default {
     getAllContacts: ctrlWrapper(getAllContacts),
@@ -88,4 +111,5 @@ export default {
     updateContact: ctrlWrapper(updateContact),
     deleteContact: ctrlWrapper(deleteContact),
     updateStatusContact: ctrlWrapper(updateStatusContact),
+    getFavoriteContacts: ctrlWrapper(getFavoriteContacts),
 }
